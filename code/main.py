9a64d20896093ca884b99582c6bdc644a2e9c792
Siddharth Saha <sahasiddharth611@gmail.com>
2021-04-16 03:41:15 +0530

    Added skeleton of gurobi optimizer in main

--- code/main.py
+++ code/main.py
@@ -16,6 +16,9 @@ class Target:
 
 class Searchers:
   def __init__(self):
+    '''
+    Single searcher for the time being
+    '''
     N = 100
     self.M = 1
     self.positions = [90]
@@ -24,18 +27,43 @@ class Searchers:
 
 class Mespp:
   def __init__(self):
+    '''
+    Grid graphs for the time being
+    '''
     N = 100
     self.g = ig.Graph.Lattice(dim=[10, 10], circular=False)
     self.target = Target()
     self.searchers = Searchers()
 
   def start(self):
-    print(self.has_captured())
-    self.plot()
+    # print(self.has_captured())
+    # self.plot()
+    self.plan()
 
   def has_captured(self):
-    # Same vertex capture for the time being
+    '''
+    Same vertex capture for the time being
+    i.e. When searcher and target are on the same vertex
+    The searcher declares "Captured!"
+    '''
     return self.target.position in self.searchers.positions
+
+  def plan(self):
+    print("Planning routine in progress...")
+
+    N = 100
+    DISCOUNT = 1
+    HORIZON = 12
+    discount_series = np.array([DISCOUNT**t for t in range(HORIZON+1)])
+
+    # To suppress gurobi optimize function's unnecessary output-
+    with gp.Env() as env, gp.Model("planner", env=env) as m:
+      beliefs = m.addMVar((N+1,HORIZON+1), lb=0, ub=1, vtype=GRB.CONTINUOUS)
+      m.setObjective(discount_series@beliefs[0,:], GRB.MAXIMIZE)
+      m.optimize()
+      print(f"Objective value: {m.objVal}")
+
+    
 
   def plot(self):
     self.g.vs["color"]="yellow"
@@ -51,5 +79,5 @@ class Mespp:
 
 
 if __name__ == '__main__':
-  print("Welcome to the Multi-robot Efficient Search Path Planning solver!")
+  print("\nWelcome to the Multi-robot Efficient Search Path Planning solver!\n")
   Mespp().start()
